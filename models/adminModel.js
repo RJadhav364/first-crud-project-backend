213d4d4aab06ab364c6613a530a0adad30beaa0b
RJadhav364 <rj8556141@gmail.com>
2025-04-18 15:03:24 +0530

    abc

--- models/adminModel.js
+++ models/adminModel.js
@@ -40,6 +40,7 @@ adminSchema.path('hasAllRights')
 
     adminSchema.methods.generateToken = async function (payload){
         try{
+            console.log(JWTKEY)
             return jwt.sign(payload,
                 process.env.JWTKEY,{
                     expiresIn: "1d",
